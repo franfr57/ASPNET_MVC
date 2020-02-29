4705914f75688e4a3b954f8e2b9dc1ed444e2df9
Schuster François <franfr2@gmail.com>
2020-02-29 21:00:05 +0100

    init

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+   public class HelloWorldController : Controller
+   {
+      /// <summary>
+      /// GET: /HelloWorld/
+      /// </summary>
+      /// <returns></returns>
+      public IActionResult Index()
+      {
+         return View();
+      }
+
+      /// <summary>
+      /// GET: HelloWorld/Welcome/3?name=Rick Requires using System.Text.Encodings.Web;
+      /// </summary>
+      /// <param name="name"></param>
+      /// <param name="numTimes"></param>
+      /// <returns></returns>
+      public IActionResult Welcome(string name, int numTimes = 1)
+      {
+         ViewData["Message"] = "Hello " + name;
+         ViewData["NumTimes"] = numTimes;
+
+         return View();
+      }
+   }
+}
